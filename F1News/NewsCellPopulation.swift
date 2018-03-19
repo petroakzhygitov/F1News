88ddabf23ae76e6902b94d9ce3f6ec8f6b5841a9
Ievgeniia Bondini <evg.bondini@gmail.com>
2018-03-19 15:41:43 -0700

    ISS-11 Load correct image after scroll

--- F1News/NewsCellPopulation.swift
+++ F1News/NewsCellPopulation.swift
@@ -3,17 +3,17 @@ import UIKit
 
 class NewsCellPopulation {
     
-    func populate(cell: UITableViewCell, newDataItem: NewDataItem)  {
+    func populate(cell: UITableViewCell, newDataItem: NewDataItem, indexPath: Int)  {
         if let newCell = cell as? NewViewCell {
             newCell.titleLabel?.text = newDataItem.title
             newCell.descriptionLabel?.text = newDataItem.description
             newCell.publishedDateLabel?.text = newDataItem.publishedDate
             
+            newCell.imgView?.sd_cancelCurrentImageLoad()
             if let url = URL(string: newDataItem.imageURL) {
                 newCell.imgView?.downloadAndSet(imageUrl: url)
             }
         }
-    }
-    
+    }    
 }
 
