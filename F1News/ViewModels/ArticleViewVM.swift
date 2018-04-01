5f32b43dd8df5d947cc7301ad1b00f4a4bbf5e66
Ievgeniia Bondini <evg.bondini@gmail.com>
2018-04-01 10:47:21 -0700

    ISS-12 Add HTML parser to ArticleVM (after merging ISS-11)

--- F1News/ViewModels/ArticleViewVM.swift
+++ F1News/ViewModels/ArticleViewVM.swift
@@ -3,10 +3,13 @@ import Foundation
 class ArticleViewVM {
     
     public var newsDataItem: NewsDataItem
+    public var htmlParser: HTMLParser
     public var urlRequest : URLRequest
     
     init(newsDataItem: NewsDataItem){
         self.newsDataItem = newsDataItem
+        self.htmlParser = HTMLParser(urlString: newsDataItem.link)
+        htmlParser.parse()
         self.urlRequest = URLRequest(url: URL.init(string: newsDataItem.link)!)
     }
 }
