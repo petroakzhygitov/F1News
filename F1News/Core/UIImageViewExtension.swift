000472b26881951f98afa6c13f80cf8edcfd5a44
Ievgeniia Bondini <evg.bondini@gmail.com>
2018-04-02 15:40:49 -0700

    ISS-25 Fix spinner on Image loading process

--- F1News/Core/UIImageViewExtension.swift
+++ F1News/Core/UIImageViewExtension.swift
@@ -5,7 +5,12 @@ import SDWebImage
 extension UIImageView {
     
     func downloadAndSet(imageUrl: URL) {
-        
-        self.sd_setImage(with: imageUrl, placeholderImage:#imageLiteral(resourceName: "placeholderImage"), options: [.progressiveDownload, .continueInBackground], completed: nil)
+        self.sd_setShowActivityIndicatorView(true)
+        self.sd_setIndicatorStyle(.gray)
+        self.sd_setImage(with: imageUrl,
+                         placeholderImage:nil,
+                         options: [.progressiveDownload,
+                                   .continueInBackground],
+                        completed: nil)
     } 
 }
