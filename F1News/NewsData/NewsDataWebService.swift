6e8cda6240d8106be55125166615769f589b7b72
Ievgeniia Bondini <evg.bondini@gmail.com>
2018-04-04 21:00:18 -0700

    ISS-28 - Timer formatter

--- F1News/NewsData/NewsDataWebService.swift
+++ F1News/NewsData/NewsDataWebService.swift
@@ -17,11 +17,32 @@ class NewsDataWebService {
             guard error == nil else {
                 return completion([], error)
             }
-            let data = dataString.data(using: .utf8)!
+            let data =  self.getFormatedDateTime(sqlDate: dataString).data(using: .utf8)!
             
             let parser: NewsDataXMLParser = NewsDataXMLParser()
             let newsDataItems: Array<NewsDataItem> = parser.parse(data: data)
             completion(newsDataItems, nil)
         }
     }
+    
+     func getFormatedDateTime(sqlDate: String?) -> String {
+        
+        if (sqlDate == nil || sqlDate == "") {
+            return ""
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH-mm-ss"
+        let date = dateFormatter.date(from: sqlDate!)
+        
+        let localFormatter = DateFormatter.dateFormat(fromTemplate: "yyyy/MM/dd HH-mm", options: 0, locale: NSLocale.current)
+        
+        dateFormatter.dateFormat = localFormatter
+        
+        
+        let outputDate = dateFormatter.string(from: date!)
+        
+        return outputDate
+    }
+
 }
