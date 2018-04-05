283afdb1cd99e948e692393b20356e814caeae55
Ievgeniia Bondini <evg.bondini@gmail.com>
2018-04-05 11:25:55 -0700

    ISS - 28 - Time Formatter for article date

--- F1News/NewsCellPopulation.swift
+++ F1News/NewsCellPopulation.swift
@@ -7,13 +7,31 @@ class NewsCellPopulation {
         if let newCell = cell as? NewViewCell {
             newCell.titleLabel?.text = newDataItem.title
             newCell.descriptionLabel?.text = newDataItem.description
-            newCell.publishedDateLabel?.text = newDataItem.publishedDate
+            newCell.publishedDateLabel?.text =  getFormatedDateTime(sqlDate: newDataItem.publishedDate)
             
             newCell.imgView?.sd_cancelCurrentImageLoad()
             if let url = URL(string: newDataItem.imageURL) {
                 newCell.imgView?.downloadAndSet(imageUrl: url)
             }
         }
-    }    
+    }
+    
+    func getFormatedDateTime(sqlDate: String?) -> String {
+        
+        if (sqlDate == nil || sqlDate == "") {
+            return ""
+        }
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ssZ"
+        let date = dateFormatter.date(from: sqlDate!)
+
+        let localFormatter = DateFormatter.dateFormat(fromTemplate: "EEE, dd MMM yyyy HH:mm", options: 0, locale: NSLocale.current)
+        dateFormatter.dateFormat = localFormatter
+        let outputDate = dateFormatter.string(from: date!)
+
+        return outputDate
+    }
+    
 }
 
--- F1News/NewsData/NewsDataWebService.swift
+++ F1News/NewsData/NewsDataWebService.swift
@@ -17,7 +17,7 @@ class NewsDataWebService {
             guard error == nil else {
                 return completion([], error)
             }
-            let data =  self.getFormatedDateTime(sqlDate: dataString).data(using: .utf8)!
+            let data = dataString.data(using: .utf8)! // self.getFormatedDateTime(sqlDate: dataString).data(using: .utf8)!
             
             let parser: NewsDataXMLParser = NewsDataXMLParser()
             let newsDataItems: Array<NewsDataItem> = parser.parse(data: data)
@@ -25,24 +25,6 @@ class NewsDataWebService {
         }
     }
     
-     func getFormatedDateTime(sqlDate: String?) -> String {
-        
-        if (sqlDate == nil || sqlDate == "") {
-            return ""
-        }
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd HH-mm-ss"
-        let date = dateFormatter.date(from: sqlDate!)
-        
-        let localFormatter = DateFormatter.dateFormat(fromTemplate: "yyyy/MM/dd HH-mm", options: 0, locale: NSLocale.current)
-        
-        dateFormatter.dateFormat = localFormatter
-        
-        
-        let outputDate = dateFormatter.string(from: date!)
-        
-        return outputDate
-    }
+
 
 }
